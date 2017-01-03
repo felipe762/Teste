786868f0553e29b01fa8ecc1f92a4ed45df22c29
felipe762 <felipedesenvolvedor@hotmail.com.br>
2017-01-03 09:55:33 -0300

    TESTE

--- BeforeTeste/src/br/com/apps/sun/paginas/VendasPageSun.java
+++ BeforeTeste/src/br/com/apps/sun/paginas/VendasPageSun.java
@@ -1,21 +1,48 @@
 package br.com.apps.sun.paginas;
 
 import br.com.utils.common.Page;
+import br.com.utils.common.fields.Dropdown;
+import br.com.utils.common.fields.SelectBox;
 
 public class VendasPageSun extends Page {
 
+	private SelectBox estado;
+	private Dropdown pdv;
+	private Dropdown vendedor;
+
 	@Override
 	public void initializeFields() {
+		estado = new SelectBox();
+		estado.set("ufVenda");
 
+		pdv = new Dropdown();
+		pdv.set("fiag_id_aux");
+
+		vendedor = new Dropdown();
+		vendedor.set("us_id_aux");
 	}
 
 	public void abrirInserirVendas() {
 		getFinder().buscaPeloId("menu_7").click();
 		getFinder().buscaPeloId("submenu_7_6").click();
-		getFinder().buscaPeloNomeDaClasse("box-header fechado").click();
+		getFinder().buscaPeloNomeDaClasse("GrupoMenuModulo22").click();
 		sleep(500);
-		getFinder().buscaPeloNomeDaClasse("botoesListaItem  corBotaoInsere botoesListaItem_fix").click();
-	}}
+		getFinder().buscaPeloNomeDaClasse("inserir-registroMenuModulo22").click();
+	}
+
+	public SelectBox getEstado() {
+		return estado;
+	}
+
+	public Dropdown getPdv() {
+		return pdv;
+	}
+
+	public Dropdown getVendedor() {
+		return vendedor;
+	}
+}
+
 
    /* public void goToTabProdutoVivoSun() {
         getFinder().buscaPeloId("vendaCelularTab").click();
